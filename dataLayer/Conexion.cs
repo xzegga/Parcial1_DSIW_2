d1dccc2b7123efb172691c09a4d9f6ee687748e6
Raúl Escamilla <raul.escamilla@asesoriait.com>
2024-08-22 22:17:33 -0600

    Data layer for Users (Get Users)

---
+++ dataLayer/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataLayer
+{
+    public class Conexion
+    {
+        public static string cnn = ConfigurationManager.ConnectionStrings["ConexionSql"].ToString();
+    }
+}
